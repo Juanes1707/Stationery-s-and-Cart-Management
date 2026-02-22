cad69c956f5963a827da3a9bbd412c98be6c278e
Juan Esteban <juan.rubio1707@gmail.com>
2026-02-22 13:23:14 -0500

    Se creo una nueva vista del historial de busqueda, al oprimir el boton de perfil guial al usuario al historial

---
+++ js/history.js
@@ -0,0 +1,9 @@
+export function renderHistory() {
+
+    const historySection = document.querySelector("#historySection");
+
+    historySection.innerHTML = `
+        <h2>Historial de Ventas</h2>
+        <p>Aquí se mostrarán las ventas guardadas.</p>
+    `;
+}
